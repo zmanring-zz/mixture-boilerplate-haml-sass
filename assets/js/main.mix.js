4f8641ad1d0218c82ab35ecb2269b838bc2747d4
rnlambert7 <nlambert@maxmedia.com>
2013-05-23 10:06:38 -0400

    Added JS to main importation

---
+++ assets/js/main.mix.js
@@ -0,0 +1,3 @@
+@import "vendor/custom.modernizr.js";
+@import "vendor/jquery-1.9.1.js";
+@import "app.js";
---
+++ assets/js/vendor/jquery-1.9.1.debug.js
@@ -0,0 +1,104 @@
+
+/Users/nlambert/Google Drive/Nick/Repositories/mixture-boilerplate-haml-sass/assets/js/vendor/jquery-1.9.1.js
+ #1 Expected exactly one space between 'function' and '('.
+    (function( window, undefined ) { // Line 14, Pos 10
+ #2 Unexpected space between '(' and 'window'.
+    (function( window, undefined ) { // Line 14, Pos 12
+ #3 Unexpected space between 'undefined' and ')'.
+    (function( window, undefined ) { // Line 14, Pos 30
+ #4 Missing 'use strict' statement.
+    var // Line 21, Pos 1
+ #5 Expected 'var' at column 5, not column 1.
+    var // Line 21, Pos 1
+ #6 Expected 'readyList' at column 13, not column 3.
+    readyList, // Line 23, Pos 3
+ #7 Expected 'rootjQuery' at column 9, not column 3.
+    rootjQuery, // Line 26, Pos 3
+ #8 Expected 'core_strundefined' at column 9, not column 3.
+    core_strundefined = typeof undefined, // Line 30, Pos 3
+ #9 Expected 'document' at column 9, not column 3.
+    document = window.document, // Line 33, Pos 3
+#10 Expected 'location' at column 9, not column 3.
+    location = window.location, // Line 34, Pos 3
+#11 Unexpected dangling '_' in '_jQuery'.
+    _jQuery = window.jQuery, // Line 37, Pos 3
+#12 Expected '_jQuery' at column 9, not column 3.
+    _jQuery = window.jQuery, // Line 37, Pos 3
+#13 Unexpected dangling '_' in '_$'.
+    _$ = window.$, // Line 40, Pos 3
+#14 Expected '_$' at column 9, not column 3.
+    _$ = window.$, // Line 40, Pos 3
+#15 Expected 'class2type' at column 9, not column 3.
+    class2type = {}, // Line 43, Pos 3
+#16 Expected 'core_deletedIds' at column 9, not column 3.
+    core_deletedIds = [], // Line 46, Pos 3
+#17 Expected 'core_version' at column 9, not column 3.
+    core_version = "1.9.1", // Line 48, Pos 3
+#18 Expected 'core_concat' at column 9, not column 3.
+    core_concat = core_deletedIds.concat, // Line 51, Pos 3
+#19 Expected 'core_push' at column 9, not column 3.
+    core_push = core_deletedIds.push, // Line 52, Pos 3
+#20 Expected 'core_slice' at column 9, not column 3.
+    core_slice = core_deletedIds.slice, // Line 53, Pos 3
+#21 Expected 'core_indexOf' at column 9, not column 3.
+    core_indexOf = core_deletedIds.indexOf, // Line 54, Pos 3
+#22 Expected 'core_toString' at column 9, not column 3.
+    core_toString = class2type.toString, // Line 55, Pos 3
+#23 Expected 'core_hasOwn' at column 9, not column 3.
+    core_hasOwn = class2type.hasOwnProperty, // Line 56, Pos 3
+#24 Expected 'core_trim' at column 9, not column 3.
+    core_trim = core_version.trim, // Line 57, Pos 3
+#25 Expected 'jQuery' at column 9, not column 3.
+    jQuery = function( selector, context ) { // Line 60, Pos 3
+#26 Expected exactly one space between 'function' and '('.
+    jQuery = function( selector, context ) { // Line 60, Pos 20
+#27 Unexpected space between '(' and 'selector'.
+    jQuery = function( selector, context ) { // Line 60, Pos 22
+#28 Unexpected space between 'context' and ')'.
+    jQuery = function( selector, context ) { // Line 60, Pos 40
+#29 Expected 'return' at column 13, not column 5.
+    return new jQuery.fn.init( selector, context, rootjQuery ); // Line 62, Pos 5
+#30 Unexpected space between '(' and 'selector'.
+    return new jQuery.fn.init( selector, context, rootjQuery ); // Line 62, Pos 32
+#31 Unexpected space between 'rootjQuery' and ')'.
+    return new jQuery.fn.init( selector, context, rootjQuery ); // Line 62, Pos 62
+#32 Expected '}' at column 9, not column 3.
+    }, // Line 63, Pos 3
+#33 Expected 'core_pnum' at column 9, not column 3.
+    core_pnum = /[+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|)/.source, // Line 66, Pos 3
+#34 Unescaped '-'.
+    core_pnum = /[+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|)/.source, // Line 66, Pos 18
+#35 Unescaped '-'.
+    core_pnum = /[+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|)/.source, // Line 66, Pos 43
+#36 Expected 'core_rnotwhite' at column 9, not column 3.
+    core_rnotwhite = /\S+/g, // Line 69, Pos 3
+#37 Expected 'rtrim' at column 9, not column 3.
+    rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, // Line 72, Pos 3
+#38 Expected 'rquickExpr' at column 9, not column 3.
+    rquickExpr = /^(?:(<[\w\W]+>)[^>]*|#([\w-]*))$/, // Line 77, Pos 3
+#39 Insecure '^'.
+    rquickExpr = /^(?:(<[\w\W]+>)[^>]*|#([\w-]*))$/, // Line 77, Pos 33
+#40 Unescaped '-'.
+    rquickExpr = /^(?:(<[\w\W]+>)[^>]*|#([\w-]*))$/, // Line 77, Pos 43
+#41 Expected 'rsingleTag' at column 9, not column 3.
+    rsingleTag = /^<(\w+)\s*\/?>(?:<\/\1>|)$/, // Line 80, Pos 3
+#42 Expected 'rvalidchars' at column 9, not column 3.
+    rvalidchars = /^[\],:{}\s]*$/, // Line 83, Pos 3
+#43 Expected 'rvalidbraces' at column 9, not column 3.
+    rvalidbraces = /(?:^|:|,)(?:\s*\[)+/g, // Line 84, Pos 3
+#44 Expected 'rvalidescape' at column 9, not column 3.
+    rvalidescape = /\\(?:["\\\/bfnrt]|u[\da-fA-F]{4})/g, // Line 85, Pos 3
+#45 Expected 'rvalidtokens' at column 9, not column 3.
+    rvalidtokens = /"[^"\\\r\n]*"|true|false|null|-?(?:\d+\.|)\d+(?:[eE][+-]?\d+|)/g, // Line 86, Pos 3
+#46 Insecure '^'.
+    rvalidtokens = /"[^"\\\r\n]*"|true|false|null|-?(?:\d+\.|)\d+(?:[eE][+-]?\d+|)/g, // Line 86, Pos 21
+#47 Unescaped '-'.
+    rvalidtokens = /"[^"\\\r\n]*"|true|false|null|-?(?:\d+\.|)\d+(?:[eE][+-]?\d+|)/g, // Line 86, Pos 73
+#48 Expected 'rmsPrefix' at column 9, not column 3.
+    rmsPrefix = /^-ms-/, // Line 89, Pos 3
+#49 Expected 'rdashAlpha' at column 9, not column 3.
+    rdashAlpha = /-([\da-z])/gi, // Line 90, Pos 3
+#50 Expected 'fcamelCase' at column 9, not column 3.
+    fcamelCase = function( all, letter ) { // Line 93, Pos 3
+#51 Too many errors. (0% scanned).
+     // Line 93, Pos 3
