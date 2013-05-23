ecda25372e538b4aaadf1a07645ce2e1f56f37ee
rnlambert7 <nlambert@maxmedia.com>
2013-05-23 11:05:08 -0400

    Added Angular and edited index introduction and example

--- assets/js/main.mix.debug.js
+++
@@ -1,6 +0,0 @@
-
-/Users/nlambert/Google Drive/Nick/Repositories/mixture-boilerplate-haml-sass/assets/js/main.mix.js
- #1 A css file should begin with @charset 'UTF-8';
-    @import "vendor/custom.modernizr.js"; // Line 1, Pos 2
- #2 Stopping.  (33% scanned).
-     // Line 1, Pos 2
